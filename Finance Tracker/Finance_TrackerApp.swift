8456dd5e2c090d59132cf997f6e7e975b97361b4
LuisLeal123 <lal10@rice.edu>
2024-05-20 21:47:37 -0500

    Initial Commit

---
+++ Finance Tracker/Finance_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_TrackerApp.swift
+//  Finance Tracker
+//
+//  Created by Luis Leal on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Finance_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
