674af1fba5247863b1f0d6f8e1b40ee71a0f22c0
LuisLeal123 <lal10@rice.edu>
2024-05-31 08:34:46 -0500

    Comments and ideas

--- Finance Tracker/Views/BudgetTrackerView.swift
+++ Finance Tracker/Views/BudgetTrackerView.swift
@@ -5,6 +5,16 @@
 //  Created by Luis Leal on 5/29/24.
 //
 
+/**
+ The 'Budget Tracker' will be a minimalistic way to monitor your budget. In any case of spending,
+ you can quickly view your remaining budget (optional to hide it) and have the ability to deduct or
+ add money depending on income or personal gain
+ */
+
+
+
+
+
 import Foundation
 import SwiftUI
 
@@ -12,7 +22,21 @@ struct BudgetTrackerView: View {
     var body: some View {
         Text("Budget Tracker")
             .font(.largeTitle)
+        
             .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(
+                LinearGradient(gradient: Gradient(colors: [
+                    Color(UIColor(red: 40/255, green: 40/255, blue: 60/255, alpha: 1.0)),
+                    Color(UIColor(red: 70/255, green: 50/255, blue: 90/255, alpha: 1.0)),
+                    Color(UIColor(red: 50/255, green: 50/255, blue: 70/255, alpha: 1.0)),
+                ]), startPoint: .bottom, endPoint: .top)
+                .overlay(
+                    Color(UIColor(red: 90/255, green: 70/255, blue: 120/255, alpha: 0.1))
+                )
+            )
+
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
